4e2d8b47cba890060a3331f6d9c41d4b1a58f17c
Rahul Ashok Kodate <115193023+RahulKodate@users.noreply.github.com>
2023-01-28 17:25:33 -0500

    Assignment 3 by TA

---
+++ Assignment-3/script.js
@@ -0,0 +1,18 @@
+//Title constructor function that creates a Title object
+function Title(t1) 
+{ this.mytitle = t1;
+}
+
+Title.prototype.getName = function () 
+{ 
+return (this.mytitle);
+}
+
+var socialMedia = {
+  facebook : 'http://facebook.com',
+  twitter: 'http://twitter.com',
+  flickr: 'http://flickr.com',
+  youtube: 'http://youtube.com'
+};
+
+var t = new Title("CONNECT WITH ME!");
